4fc97a6c09bfd977c73ce4c048cb40271218563f
delaneykiley <102424132+delaneykiley@users.noreply.github.com>
2024-07-26 17:44:15 -0400

    Update pie_chart.js

--- pie_chart.js
+++ pie_chart.js
@@ -25,27 +25,27 @@ var yearsDict = {"2015-2020": 256, "2005-2009": 161, "1995-1999": 101, "2010-201
                  "2000-2004": 147, "1990-1994": 60, "1980-1984": 8, "1975-1979": 8, 
                  "1985-1989": 25, "1970-1974": 2, "1935-1939": 2, "1940-1944": 1, "1960-1964": 1, "1965-1969": 1}
 
-var data2 = {a: 6, b: 16, c:20, d:14, e:19, f:12}
 
-// set the color scale
+// set the color scale to given scheme
 var color = d3.scaleOrdinal()
   .domain(year_band_labels)
   .range(d3.schemeDark2);
 
-// A function that create / update the plot for a given variable:
+// create and update the pie chart for input data variable
 function update(data) {
 
-  // Compute the position of each group on the pie:
+
   var pie = d3.pie()
     .value(function(d) {return d.value; })
     .sort(function(a, b) { console.log(a) ; return d3.ascending(a.key, b.key);} ) // This make sure that group order remains the same in the pie chart
   var data_ready = pie(d3.entries(data))
 
-  // map to data
+
+    
   var u = svg.selectAll("path")
     .data(data_ready)
 
-  // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+    
   u
     .enter()
     .append('path')
@@ -61,57 +61,15 @@ function update(data) {
     .style("stroke-width", "2px")
     .style("opacity", 1)
 
-  // remove the group that is not present anymore
+    
   u
     .exit()
     .remove()
 
 }
 
-// Initialize the plot with the first dataset
+// Initialize the plot with the dataset
 update(yearsDict)
 
-    
-// helper function to find release year band for film
-function findReleaseYearBand(yr) {
-
-        var band20 = ["1920", "1921", "1922", "1923", "1924"];
-        var band25 = ["1925", "1926", "1927", "1928", "1929"];
-        var band30 = ["1930", "1931", "1932", "1933", "1934"];
-        var band35 = ["1935", "1936", "1937", "1938", "1939"];
-        var band40 = ["1940", "1941", "1942", "1943", "1944"];
-        var band45 = ["1945", "1946", "1947", "1948", "1949"];
-        var band50 = ["1950", "1951", "1952", "1953", "1954"];
-        var band55 = ["1955", "1956", "1957", "1958", "1959"];
-        var band60 = ["1960", "1961", "1962", "1963", "1964"];
-        var band65 = ["1965", "1966", "1967", "1968", "1969"];
-        var band70 = ["1970", "1971", "1972", "1973", "1974"];
-        var band75 = ["1975", "1976", "1977", "1978", "1979"];
-        var band80 = ["1980", "1981", "1982", "1983", "1984"];
-        var band85 = ["1985", "1986", "1987", "1988", "1989"];
-        var band90 = ["1990", "1991", "1992", "1993", "1994"];
-        var band95 = ["1995", "1996", "1997", "1998", "1999"];
-        var band00 = ["2000", "2001", "2002", "2003", "2004"];
-        var band05 = ["2005", "2006", "2007", "2008", "2009"];
-        var band10 = ["2010", "2011", "2012", "2013", "2014"];
-        var band15 = ["2015", "2016", "2017", "2018", "2019"];
-
-      var options = [band20, band25, band30, band35, band40, band45, band50, band55, band60, band65, 
-                     band70, band75, band80, band85, band90, band95, band00, band05, band10, band15];
-
-      for (var i = 0; i < options.length; i++) {
-
-        if (options[i].includes(yr)) { return year_band_labels[i]; }
-      }
-
-      return "Other";
-  }
-
-// helper function to build release year dictionary
-function buildYearsDict(data) {
-    yearsDict = {};
-    // build dictionary here
-    return yearsDict;
-}
 
 // structure of graph inspired by information at https://d3-graph-gallery.com/graph/pie_changeData.html
