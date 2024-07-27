2556d6e6a901be12513fd5ca0b328afb55716bdf
delaneykiley <102424132+delaneykiley@users.noreply.github.com>
2024-07-27 18:31:08 -0400

    Update packing_chart.js

--- packing_chart.js
+++ packing_chart.js
@@ -6,7 +6,7 @@ var margin = {top: 50, right: 60, bottom: 160, left: 150},
     height = 500 - margin.top - margin.bottom;
 
 // create svg object and append to "#chart3"
-var svg = d3.select("chart3")
+var svg = d3.select("#chart3")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
