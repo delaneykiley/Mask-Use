bea648550bc88302672c3bb39239ba61aaa8f5c9
delaneykiley <102424132+delaneykiley@users.noreply.github.com>
2024-07-27 18:50:33 -0400

    Update packing_chart.js

--- packing_chart.js
+++ packing_chart.js
@@ -1,12 +1,12 @@
 
 
-// set margin and dimensions of graph
-var margin = {top: 50, right: 60, bottom: 160, left: 150},
-    width = 1200 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
-// create svg object and append to "#chart3"
-var svg = d3.select("#chart3")
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -14,101 +14,34 @@ var svg = d3.select("#chart3")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-
-// Read data
+//Read the data
 d3.csv("ind_year_data.csv", function(data) {
 
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([1920, 2020])
+    .range([ 0, width ]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
 
-  // Color palette for continents?
-  var color = d3.scaleOrdinal()
-    .domain(1920,2020)
-    .range(d3.schemeSet1);
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, 1000])
+    .range([ height, 0]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
 
-  // Size scale for countries
-  var size = d3.scaleLinear()
-    .domain([0, 1400000000])
-    .range([7,55])  // circle will be between 7 and 55 px wide
-
-  // create a tooltip
-  var Tooltip = d3.select("#chart3")
-    .append("div")
-    .style("opacity", 0)
-    .attr("class", "tooltip")
-    .style("background-color", "white")
-    .style("border", "solid")
-    .style("border-width", "2px")
-    .style("border-radius", "5px")
-    .style("padding", "5px")
-
-  // Three function that change the tooltip when user hover / move / leave a cell
-  var mouseover = function(d) {
-    Tooltip
-      .style("opacity", 1)
-  }
-  var mousemove = function(d) {
-    Tooltip
-      .html('<u>' + d.Year + '</u>' + "<br>" + d.Count)
-      .style("left", (d3.mouse(this)[0]+20) + "px")
-      .style("top", (d3.mouse(this)[1]) + "px")
-  }
-  var mouseleave = function(d) {
-    Tooltip
-      .style("opacity", 0)
-  }
-
-  // Initialize the circle: all located at the center of the svg area
-  var node = svg.append("g")
-    .selectAll("circle")
+  // Add dots
+  svg.append('g')
+    .selectAll("dot")
     .data(data)
     .enter()
     .append("circle")
-      .attr("class", "node")
-      .attr("r", function(d){ return size(d.Count)})
-      .attr("cx", width / 2)
-      .attr("cy", height / 2)
-      .style("fill", function(d){ return color(d.Year)})
-      .style("fill-opacity", 0.8)
-      .attr("stroke", "black")
-      .style("stroke-width", 1)
-      .on("mouseover", mouseover) // What to do when hovered
-      .on("mousemove", mousemove)
-      .on("mouseleave", mouseleave)
-      .call(d3.drag() // call specific function when circle is dragged
-           .on("start", dragstarted)
-           .on("drag", dragged)
-           .on("end", dragended));
-
-  // Features of the forces applied to the nodes:
-  var simulation = d3.forceSimulation()
-      .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
-      .force("charge", d3.forceManyBody().strength(.1)) // Nodes are attracted one each other of value is > 0
-      .force("collide", d3.forceCollide().strength(.2).radius(function(d){ return (size(d.value)+3) }).iterations(1)) // Force that avoids circle overlapping
-
-  // Apply these forces to the nodes and update their positions.
-  // Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
-  simulation
-      .nodes(data)
-      .on("tick", function(d){
-        node
-            .attr("cx", function(d){ return d.x; })
-            .attr("cy", function(d){ return d.y; })
-      });
-
-  // What happens when a circle is dragged?
-  function dragstarted(d) {
-    if (!d3.event.active) simulation.alphaTarget(.03).restart();
-    d.fx = d.x;
-    d.fy = d.y;
-  }
-  function dragged(d) {
-    d.fx = d3.event.x;
-    d.fy = d3.event.y;
-  }
-  function dragended(d) {
-    if (!d3.event.active) simulation.alphaTarget(.03);
-    d.fx = null;
-    d.fy = null;
-  }
+      .attr("cx", function (d) { return x(d.Year); } )
+      .attr("cy", function (d) { return y(d.Count); } )
+      .attr("r", 1.5)
+      .style("fill", "#69b3a2")
 
 })
 
