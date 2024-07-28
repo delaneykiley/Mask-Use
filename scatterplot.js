d56ec23649223395e944ed203e770525413bb378
delaneykiley <102424132+delaneykiley@users.noreply.github.com>
2024-07-28 14:37:35 -0400

    Update scatterplot.js

--- scatterplot.js
+++ scatterplot.js
@@ -20,28 +20,18 @@ const yearData = d3.csv("ind_year_data.csv");
 yearData.then( function(data) {
 
   // Add X axis
-  // var x = d3.scaleLinear()
-  //   .domain(data.map(function(d) { return d.Year}))
-  //   .range([ 0, width ]);
-    var x = d3.time.scale()
-        .range([0, width]);
+  var x = d3.scaleLinear()
+    .domain(data.map(function(d) { return d.Year}))
+    .range([ 0, width ]);
 
-    x.domain(data.map(function(d) { return d.Year}))
-
-     var xAxis = d3.svg.axis()
-              .scale(x)
-              .orient("bottom")
-              .tickFormat(d3.time.format("%Y"));
-    svg.append("g")
-                      .attr("class", "x axis")
-                      .attr("transform", "translate(0," + height + ")")
-                      .call(xAxis);
     
-  // svg.append("g")
-    // .attr("transform", "translate(0," + height + ")")
-    // .call(d3.axisBottom(x));
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x).ticks(d3.timeYear.every(1)));
 
+    
     var counts = d3.map(data, function(d){ return d.Count; }).keys();
+    
   // Add Y axis
   var y = d3.scaleLinear()
     .domain(0, 60)
