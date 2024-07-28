3eb270b7b155953403250d89c7ad95b5cecaee0d
delaneykiley <102424132+delaneykiley@users.noreply.github.com>
2024-07-28 15:26:45 -0400

    Update scatterplot.js

--- scatterplot.js
+++ scatterplot.js
@@ -13,11 +13,11 @@ var svg = d3.select("#chart3")
           "translate(" + margin.left + "," + margin.top + ")");
 
 //Read the data
-d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
+d3.csv("ind_year_data.csv",
 
   // When reading the csv, I must format variables:
   function(d){
-    return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
+    return { date : d3.timeParse("%Y-%m-%d")(d.Year), value : d.Count }
   },
 
   // Now I can use this dataset:
