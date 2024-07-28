a29b4a4526e8aa93a51f01bfc5223bd580884ee8
delaneykiley <102424132+delaneykiley@users.noreply.github.com>
2024-07-28 16:29:44 -0400

    Update scatterplot.js

--- scatterplot.js
+++ scatterplot.js
@@ -1,12 +1,10 @@
-
-
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 30, left: 60},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
-var svg = d3.select("#chart3")
+var svg = d3.select("#my_dataviz")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -15,33 +13,40 @@ var svg = d3.select("#chart3")
           "translate(" + margin.left + "," + margin.top + ")");
 
 //Read the data
-d3.csv("ind_year_data.csv", function(data) {
+d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
 
-  // Add X axis
-  var x = d3.scaleTime()
-    .domain([new Date(1920, 0, 1), new Date(2021, 0, 1)])
-    .rangeRound([20,width - 20]);
-    var xAxis = d3.axisBottom(x).tickFormat(d3.timeFormat("%Y"));
-  svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
+  // When reading the csv, I must format variables:
+  function(d){
+    return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
+  },
 
-  // Add Y axis
-  var y = d3.scaleLinear()
-    .domain([0, 60])
-    .range([ height, 0]);
-  svg.append("g")
-    .call(d3.axisLeft(y));
+  // Now I can use this dataset:
+  function(data) {
 
-  // Add dots
-  svg.append('g')
-    .selectAll("dot")
-    .data(data)
-    .enter()
-    .append("circle")
-      .attr("cx", function (d) { return x(d.Year); } )
-      .attr("cy", function (d) { return y(d.Count); } )
-      .attr("r", 1.5)
-      .style("fill", "#69b3a2")
+    // Add X axis --> it is a date format
+    var x = d3.scaleTime()
+      .domain(d3.extent(data, function(d) { return d.date; }))
+      .range([ 0, width ]);
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain([0, d3.max(data, function(d) { return +d.value; })])
+      .range([ height, 0 ]);
+    svg.append("g")
+      .call(d3.axisLeft(y));
+
+    // Add the line
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.line()
+        .x(function(d) { return x(d.date) })
+        .y(function(d) { return y(d.value) })
+        )
 
 })
