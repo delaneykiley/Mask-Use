7ccf89e8340eaecd5191177498ea3c4531b95e4c
delaneykiley <102424132+delaneykiley@users.noreply.github.com>
2024-07-28 16:55:10 -0400

    Update scatterplot.js

--- scatterplot.js
+++ scatterplot.js
@@ -11,3 +11,37 @@ var svg = d3.select("#chart3")
   .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
+
+const years_data = d3.csv("/data/cities.csv");
+years_data.then(
+    function(d){
+    return { date : d3.timeParse("%Y")(d.Year), value : d.Count }
+  },
+    function(data) {
+    console.log
+    // Add X axis --> it is a date format
+    var x = d3.scaleTime()
+      .domain(d3.extent(data, function(d) { return d.date; }))
+      .range([ 0, width ]);
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain([0, d3.max(data, function(d) { return +d.value; })])
+      .range([ height, 0 ]);
+    svg.append("g")
+      .call(d3.axisLeft(y));
+
+    // Add the line
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.line()
+        .x(function(d) { return x(d.date) })
+        .y(function(d) { return y(d.value) })
+        )
+});
